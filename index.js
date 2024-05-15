01d95d947f76696a8517d2c681c9294de4d2a3dd
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-05-15 11:32:27 +0500

    tuagadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting");
+let id = prompt("Space idingizni kiriting");
+let poss = prompt("Space posswordinzgini kiriting");
+
+let data = {
+    ism: ism,
+    id: id,
+    poss: poss,
+}
+
+console.log(data);
